d0f94b23998d842af1f30d91f287385cae3aad86
NaincyShrivastava05 <naincyshri05@gmail.com>
2025-03-04 12:29:56 +0530

    [Naincy] add JS AddressBook Ability to ensure Valid Contacts are added with the help of Regex

--- AddressBook.js
+++ AddressBook.js
@@ -1,5 +1,24 @@
 class Contact {
   constructor(firstName, lastName, address, city, state, zip, phone, email) {
+    let zipRegex = /^\d{6}$/;
+    let phoneRegex = /^[7-9]\d{9}$/;
+    let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
+        throw new Error("Invalid Name! Should start with a capital and be at least 3 characters.");
+    }
+    if (!addressRegex.test(address) || !addressRegex.test(city) || !addressRegex.test(state)) {
+        throw new Error("Invalid Address, City or State! Should have at least 4 characters.");
+    }
+    if (!zipRegex.test(zip)) {
+        throw new Error("Invalid Zip! Should be exactly 6 digits.");
+    }
+    if (!phoneRegex.test(phone)) {
+        throw new Error("Invalid Phone Number! Should be a 10-digit number starting with 7, 8, or 9.");
+    }
+    if (!emailRegex.test(email)) {
+        throw new Error("Invalid Email Address!");
+    }
       this.firstName = firstName;
       this.lastName = lastName;
       this.address = address;
@@ -11,6 +30,9 @@ class Contact {
   }
 }
 
-
+try{
 let contact1 = new Contact("Naincy", "Shrivastava", "Trilanga", "Bhopal", "Madhya Pradesh", "402938", "1234753422", "naincyshri0502@example.com");
 console.log("Contact Created:", contact1);
+}catch (error) {
+  console.log(error.message);
+}
